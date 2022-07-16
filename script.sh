919632023a57f321766b85eb71a7187fddf2bea3
Thet Paing Hein <paiakarit@gmail.com>
2022-07-16 23:12:35 +0630

    update script.sh (#3)

--- script.sh
+++ script.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-function hidePid() {
-   mount --bind /dev/shm/.pid /proc/$pid
+
+PID=
+PROGNAME="$(basename $0)"
+GETOPT_ARGS=$(getopt -o hc:w:g:de:nm: -l "help","pid:" -n "$PROGNAME" -- "$@")
+
+
+function usage() {
+  echo "Usage: "$PROGNAME" [Options] <Process-ID>"
+  echo
+  echo "Options: "
+  echo "    -h, --help        Display this help and exit"
+  echo "    --pid             Process ID"
 }
+
+
+function hidePid() {
+   mount --bind /dev/shm/.pid /proc/$PID
+}
+
 
 function mkDir() {
   rm -rf /dev/shm/.pid
@@ -14,20 +30,51 @@ function mkDir() {
   fi
 }
 
+
+[[ $? -ne 0 ]] && exit 1
+eval set -- "$GETOPT_ARGS"
+
+
+while :; do
+  case "$1" in
+    -h|--help)
+      usage
+      exit 0
+    ;;
+    --pid)
+      shift
+      PID="$1"
+      shift
+    ;;
+    --)
+      shift
+      break
+    ;;
+  esac
+done
+
+
 if [[ $(id -u) -ne "0" ]]; then
   echo "[error] You must run this script as root" >&2
   exit 1
 fi
 
-echo -ne "\r"
-read -p "Process ID: " pid
 
-mesg="Your PID was successfully hidden!"
+if [[ -n "$PID" ]]; then
+  msg="You Process ID was successfully hidden!"
 
-mkDir && hidePid && \
+  mkDir && hidePid && \
 
-for i in $(seq 1 ${#mesg}); do
-  echo -ne "${mesg:i-1:1}"
-  sleep 0.07
-done
-echo -ne "\n"
+  for i in $(seq 1 ${#msg}); do
+    echo -ne "${msg:i-1:1}"
+    sleep 0.06
+  done
+
+  exit 1
+fi
+
+
+if [[ $# -lt 1 ]]; then
+    usage >&2
+    exit 1
+fi
